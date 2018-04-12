4eb69535a539c3de09b552cd8fa6cb0e519bafe9
Gaëtan Deschamps <gaetan.deschamps@hei.yncrea.fr>
2018-04-12 11:36:46 +0200

    commit

--- src/main/java/hei/tresorock/servlet/ModeTEA.java
+++ src/main/java/hei/tresorock/servlet/ModeTEA.java
@@ -44,7 +44,7 @@ public class ModeTEA extends HttpServlet {
 
         //accès uniquement si un évènement a été créé côté ADMIN
 
-            //IMPLEMENTER METHODE -marche
+            //IMPLEMENTER METHODE  TOP
 
     }
 
